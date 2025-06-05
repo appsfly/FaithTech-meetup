0e8cdc792bb1f3eea2c6aabaf99e4a4b9d51712a
GidiSayDo <80324378+GidiSayDo@users.noreply.github.com>
2025-06-05 12:59:28 +0300

    wip

--- src/Component/StudentHeader/index.tsx
+++ src/Component/StudentHeader/index.tsx
@@ -76,20 +76,25 @@ export default function Header({ title }: { title: string }) {
       {location.pathname !== "/" ? (
         backDisplay
       ) : (
-        <LogoutIcon
+        <div
           onClick={logout}
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-          strokeWidth={2}
+          style={{ display: "flex", alignItems: "center", gap: 10 }}
         >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H6a2 2 0 01-2-2V7a2 2 0 012-2h5a2 2 0 012 2v1'
-          />
-        </LogoutIcon>
+          <LogoutIcon
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H6a2 2 0 01-2-2V7a2 2 0 012-2h5a2 2 0 012 2v1'
+            />
+          </LogoutIcon>
+          <Title>logout</Title>
+        </div>
       )}
     </HeaderWrapper>
   );
